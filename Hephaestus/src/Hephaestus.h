5038e86fa25f354be6297003604c74bfaf16035b
Kethers <838859010@qq.com>
2023-11-16 01:54:43 +0800

    finish Logging

--- Hephaestus/src/Hephaestus.h
+++ Hephaestus/src/Hephaestus.h
@@ -3,6 +3,7 @@
 // For use by Hephaestus applications
 
 #include "Hephaestus/Application.h"
+#include "Hephaestus/Log.h"
 
 // -------- Entry Point --------
 #include "Hephaestus/EntryPoint.h"
--- Hephaestus/src/Hephaestus/EntryPoint.h
+++ Hephaestus/src/Hephaestus/EntryPoint.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Application.h"
+#include "Log.h"
 
 #ifdef HEP_PLATFORM_WINDOWS
 
@@ -7,6 +8,11 @@ extern Hep::Application* Hep::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hep::Log::Init();
+	HEP_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HEP_INFO("Hello! Var={0}", a);
+
 	auto app = Hep::CreateApplication();
 	app->Run();
 	delete app;
